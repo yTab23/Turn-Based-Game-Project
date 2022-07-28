498bdd345df194dd8557c3e8acbe344834866bfd
Yuki Tabata <ytabata11@gmail.com>
2022-07-28 12:20:18 +1000

    Unit will play the walking animation when they are moving to their destination.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
     private void Update() 
     {
+
+
         float stoppingDistance = 0.1f;
         if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
         if(Input.GetMouseButtonDown(0))
