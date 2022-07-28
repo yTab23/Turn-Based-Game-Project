69b998f9d10e07cb8f3ad5ee307bd5f1c67183fc
Yuki Tabata <ytabata11@gmail.com>
2022-07-28 12:26:03 +1000

    The unit will rotate smoothly when they head towards their destination.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -17,6 +17,8 @@ public class Unit : MonoBehaviour
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
             unitAnimator.SetBool("IsWalking", true);
         }
         else
