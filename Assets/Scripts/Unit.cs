59e97f2acfd0bb43c86730626e6152c7c34bf44c
Yuki Tabata <ytabata11@gmail.com>
2022-07-29 18:23:59 +1000

    BaseAction abstract class created which will provide the template for all actions in the game such as movement, spinning and shooting etc.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,10 +6,12 @@ public class Unit : MonoBehaviour
 {
     private GridPosition gridPosition;
     private MoveAction moveAction;
+    private SpinAction spinAction;
 
     private void Awake() 
     {
         moveAction = GetComponent<MoveAction>();
+        spinAction = GetComponent<SpinAction>();
     }
 
     private void Start() 
@@ -31,6 +33,11 @@ public class Unit : MonoBehaviour
     public MoveAction GetMoveAction()
     {
         return moveAction;
+    }
+
+    public SpinAction GetSpinAction()
+    {
+        return spinAction;
     }
 
     public GridPosition GetGridPosition()
