e7af74421f696c7261b87e0d7ce62c470fd7fecc
Yuki Tabata <ytabata11@gmail.com>
2022-07-30 22:32:12 +1000

    Health and action points UI displayed in the game world.

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 public class HealthSystem : MonoBehaviour
 {
     public event EventHandler OnDead;
+    public event EventHandler OnDamaged;
 
     [SerializeField] private int health = 100;
+    private int healthMax = 100;
+
+    private void Awake() 
+    {
+        healthMax = health;
+    }
 
     public void Damage(int damageAmount)
     {
@@ -17,6 +24,8 @@ public class HealthSystem : MonoBehaviour
         {
             health = 0;
         }
+
+        OnDamaged?.Invoke(this, EventArgs.Empty);
 
         if (health == 0)
         {
@@ -29,5 +38,10 @@ public class HealthSystem : MonoBehaviour
     private void Die()
     {
         OnDead?.Invoke(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalized()
+    {
+        return (float)health / healthMax;
     }
 }
