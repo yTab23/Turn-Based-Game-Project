571fbe5ee268dba8950f8545390b5e9ee25641f1
Yuki Tabata <ytabata11@gmail.com>
2022-08-02 14:12:15 +1000

    Input refactored. Input manager implemented so all interactions are centralized so it easy to manage.

--- Assets/Scripts/Grid/CameraController.cs
+++ Assets/Scripts/Grid/CameraController.cs
@@ -27,58 +27,28 @@ public class CameraController : MonoBehaviour
 
     private void HandleMovement()
     {
-        Vector3 inputMoveDir = new Vector3(0, 0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputMoveDir.z = +1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputMoveDir.z = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputMoveDir.x = +1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputMoveDir.x = -1f;
-        }
+        Vector2 inputMoveDir = InputManager.Instance.GetCameraMoveVector();
 
         float moveSpeed = 10f;
-        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+        Vector3 moveVector = transform.forward * inputMoveDir.y + transform.right * inputMoveDir.x;
         transform.position += moveVector * moveSpeed * Time.deltaTime;
     }
 
     private void HandleRotation()
     {
         Vector3 rotationVector = new Vector3(0, 0, 0);
+        
+        rotationVector.y = InputManager.Instance.GetCameraRotateAmount();
 
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotationVector.y = +1f;
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            rotationVector.y = -1f;
-        }
         float rotationSpeed = 100f;
         transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
     }
 
     private void HandleZoom()
     {
-        float zoomAmount = 1f;
-        if (Input.mouseScrollDelta.y > 0)
-        {
-            targetFollowOffset.y -= zoomAmount;
+        float zoomIncreaseAmount = 1f;
+        targetFollowOffset.y += InputManager.Instance.GetCameraZoomAmount() * zoomIncreaseAmount;
 
-        }
-        if (Input.mouseScrollDelta.y < 0)
-        {
-            targetFollowOffset.y += zoomAmount;
-        }
         targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
 
         float zoomSpeed = 5f;
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one InputManager! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public Vector2 GetMouseScreenPosition()
+    {
+        return Input.mousePosition;
+    }
+
+    public bool IsMouseButtonDown()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    public Vector2 GetCameraMoveVector()
+    {
+        Vector2 inputMoveDir = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMoveDir.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputMoveDir.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveDir.x = +1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMoveDir.x = -1f;
+        }
+
+        return inputMoveDir;
+    }
+
+    public float GetCameraRotateAmount()
+    {
+        float rotateAmount = 0f;
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            rotateAmount = +1f;
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            rotateAmount = -1f;
+        }
+
+        return rotateAmount;
+    }
+
+    public float GetCameraZoomAmount()
+    {
+        float zoomAmount = 0f;
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            zoomAmount = -1f;
+
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            zoomAmount = +1f;
+        }
+        return zoomAmount;
+    }
+}
